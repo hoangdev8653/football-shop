32972a530e6dccf35daf613accdaceacc2b745bd
hhoang777 <hhoang1072003@gmail.com>
2025-01-22 23:42:28 +0700

    add file localStorage

---
+++ client/src/utils/localStorage.js
@@ -0,0 +1,13 @@
+export const setLocalStorage = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+  
+  export const getLocalStorage = (key) => {
+    const value = JSON.parse(localStorage.getItem(key));
+    return value;
+  };
+  
+  export const clearLocalStorage = () => {
+    return localStorage.clear();
+  };
+  
